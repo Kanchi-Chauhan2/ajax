3e4ec9d7da2ffe615bd31fb5783cca8a166d259b
kanchi chauhan <78559293+Kanchi-Chauhan2@users.noreply.github.com>
2021-06-05 13:01:25 +0530

    Update callbacks.js

--- js/callbacks.js
+++ js/callbacks.js
@@ -1,9 +1,92 @@
+
+/////////////////////      SUM     ////////////////////////////
+
+function display(a){
+    document.body.innerHTML=`${a}`;
+}
+
+function sum(x,y,callback){
+    console.log('sum');
+    let a=x+y;
+    callback(a);
+}
+sum(1,2,display);
+
+
+
+
+
+/////////////////////       image transitions          /////////////////////////////
+
+
+
+
+
+/*
+var imgArray = new Array();
+
+imgArray[0] = new Image();
+imgArray[0].src = '../img/about.jpg';
+
+imgArray[1] = new Image();
+imgArray[1].src = '../img/about-banner.jpg';
+
+imgArray[2] = new Image();
+imgArray[2].src = '../img/flag_america.jpg';
+
+document.querySelector('.container').src=imgArray[0].src;
+
+console.log('finished');
+
+*/
+
+
+
+
+//////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+/*
+array=[
+    {title:"array one"},
+    {title:"array two"}
+];
+
+function createPost(post){
+    setTimeout(()=>{
+        array.push(post);
+    },2000);
+}
+function getPost(){
+    setTimeout(()=>{
+        let output='';
+    array.forEach(element => {
+        output+=`<li>${element.title}</li>`
+    });
+    document.body.innerHTML=output;
+    },1000);
+    
+}
+getPost();
+
+createPost({title:"array three"});
+
+console.log(array);
+
+*/
+
+
+
 ///////////////////////      PRACTICE         ////////////////////////
 
 
 
 
-
+/*
 const posts=[
     {title:'post one',body:'this is post one'},
     {title:'post one',body:'this is post two'}
@@ -27,28 +110,7 @@ function getPost(){
 }
 createPost({title:'post 3',body:'asdfggggggggg'},getPost);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+*/
 
 
 ///////////////////////       BRAD TRAVERSY        //////////////////////////
