9c45d3b93da068510467a2e0da3a68ef187d002a
kanchi chauhan <78559293+Kanchi-Chauhan2@users.noreply.github.com>
2021-06-09 23:51:34 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,11 +1,46 @@
-const http=new easyHttp;
+const http=new easyhttp();
 
-http.get('https://jsonplaceholder.typicode.com/posts1',
+/*http.get('https://jsonplaceholder.typicode.com/posts/1',
 function(err,response){
     if(err){
         console.log(err);
     }else{
         console.log(response);
     }
-});
+    
+})*/
 
+const data={
+    title:'post',
+    body:'this is a custom post'
+};
+
+/*http.post('https://jsonplaceholder.typicode.com/posts/1',data,
+function(err,response){
+    if(err){
+        console.log(err);
+    }else{
+        console.log(response);
+    }
+    
+})*/
+
+/*http.put('https://jsonplaceholder.typicode.com/posts/1',data,
+function(err,response){
+    if(err){
+        console.log(err);
+    }else{
+        console.log(response);
+    }
+    
+})*/
+
+http.delete('https://jsonplaceholder.typicode.com/posts/1',data,
+function(err,response){
+    if(err){
+        console.log(err);
+    }else{
+        console.log(response);
+    }
+    
+})
