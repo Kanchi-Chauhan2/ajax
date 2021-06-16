32131581b1b9de7f1dbc3a0d831d57b30cad89fe
kanchi chauhan <78559293+Kanchi-Chauhan2@users.noreply.github.com>
2021-06-16 16:20:16 +0530

    Update promises.js

--- js/promises.js
+++ js/promises.js
@@ -1,94 +1,154 @@
-/////////////////////////    PROMISES     //////////////////////////
+////////////////////////    CALLBACK     ////////////////////////
 
 
 
-/*
-function display(some){
-    document.body.innerHTML=some;
+
+
+/*function user(email,phone,callback){
+    setTimeout(()=>{
+        console.log("I entered here");
+        callback({usermail:email,userphone:phone}) ;
+    },3000);
 }
 
-let promise=new Promise(function(resolve,reject){
-    
-    let x=0;
+function getVideos(email,callback){
+    setTimeout(()=>{
+        callback(["video1","video2","video3"]);
+    },1000)
+}
 
-    if(x==0){
-        resolve("okk");
-    }
-    else{
-        reject("error");
-    }
+function getanything(videos,callback){
+    setTimeout(()=>{
+        callback("title of the video");
+    },2000);
+}
+
+
+
+/////////////  CALLBACKS MAKES IT NESTED  /////////////
+
+
+
+const userfunc=user('ghjkl',22222,(user)=>{
+    console.log(user.userphone);
+    getVideos(user.usermail,videos=>{
+        console.log(videos);
+        getanything(videos[0],title=>{
+            console.log(title);
+        })
+    })
 });
 
-promise.then(
-    function(value){
-        display(value);
-    },
-    function(error){
-        display(error);
-    }
-)*/
+
+
+console.log(userfunc);*/
 
 
 
-///////////////////         practice by simple example       /////////////////////
-
-
+////////////////////////////      PROMISES       /////////////////////////////
 
 /*
 
-let promise=new Promise(function(resolve,reject){
+function user(email,phone){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            console.log("I entered here");
+            resolve({usermail:email,userphone:phone}) ;
+        },3000);
+    });
+}
 
-    let x=8;
+function getVideos(email){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            resolve(["video1","video2","video3"]);
+        },1000)
+    });
+   
+}
 
-    if(x==0){
-        resolve("okk");
-    }
-    else{
-        reject("error");
-    }
-});
+function getanything(videos){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            resolve("title of the video");
+        },2000);
+    });
+   
+}
 
-promise.then(
-    function(value){
-        document.body.innerHTML=value;
-        console.log(value);
-    },
-    function(error){
-        document.body.innerHTML=error;
-        console.log(error);
-    }
-)
 
+user("ghjkl","22222")
+.then(user=>getVideos(user.email))
+.then(videos=>getanything(videos))
 */
 
 
 
-////////////////////////     promises in callback   /////////////////////////
+///////////////////////////     Promises     ////////////////////
+
+/*var promise=new Promise(function(resolve,reject){
+    const x=1;
+    const y=2;
+
+    if(x===y){
+        resolve();
+    }
+    else
+    {
+        reject();
+    }
+});
+
+promise
+.then(function(){
+    console.log("resolved");
+},function(){
+    console.log("rejected");
+})*/
 
 
-let posts=[
-    {title:"post 1", body:"this is post one"},
-    {title:"post 2", body:"this is post two"}
-];
+/*
+var promise=new Promise(function(resolve,reject){
+    reject('resolved');
+})
 
-function createpost(post,callback){
-    return new Promise(function(resolve,reject){
-        setTimeout(function(){
-            posts.push(post);
-            resolve();
-        },3000);
-    } );
-}
+promise
+.then(function(success){
+    console.log(success);
+},function(){
+    console.log(new Error("error"));
+})
+*/
 
-function getPost(){
+
+/////////////////////////      Promise.resolve    ////////////////////////
+
+
+
+/*var p=Promise.resolve(12345);
+
+p.then(function(val){
+    console.log(val);
+});
+
+var q=new Promise((resolve,reject)=>{
     setTimeout(()=>{
-        let output='';
-        posts.forEach((post)=>{
-        output+=`<li>${post.title}</li>`;
-    })
-    document.body.innerHTML=output;
-    },1000);  
-}
+        resolve([11,12,13]);
+    },2000);
+});
 
-createpost({title:"post3", body:"post three"})
-.then(getPost);
+q.then(function(values){
+    console.log(values);
+});
+
+const r=Promise.resolve(43255);
+
+const res=new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        r.then(val=>console.log(val));
+    },1000);
+});
+
+res.then((valss)=>{
+    console.log(valss);
+})*/
